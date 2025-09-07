791a8c00a58b3897030908436841ad2b7ca3c73a
kacerns <karoliscernevicius@gmail.com>
2025-09-07 16:32:44 +0300

    feat(NewsDetails): -Added News Details screen, which can be accessed when clicking on a news article widget.

--- lib/features/NewsApp/presentation/widgets/article_card_widget.dart
+++ lib/features/NewsApp/presentation/widgets/article_card_widget.dart
@@ -1,6 +1,7 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:newsapp/features/NewsApp/domain/entities/news_article_entity.dart';
+import 'package:newsapp/features/NewsApp/presentation/screens/news_details_screen.dart';
 
 class ArticleCard extends StatelessWidget {
   final NewsArticleEntity newsArticleEntity;
@@ -38,87 +39,100 @@ class ArticleCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double deviceHeight = MediaQuery.of(context).size.height;
-    double deviceWidth = MediaQuery.of(context).size.width;
+    double deviceHeight = MediaQuery.sizeOf(context).height;
+    double deviceWidth = MediaQuery.sizeOf(context).width;
 
-    return Card(
-      margin: EdgeInsets.symmetric(
-        horizontal: deviceWidth * 0.03,
-        vertical: deviceHeight * 0.01,
-      ),
-      child: Container(
-        height: deviceHeight >= deviceWidth
-            ? deviceHeight * 0.25
-            : deviceHeight * 0.5,
-        padding: EdgeInsets.symmetric(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) =>
+                NewsDetailsScreen(newsArticleEntity: newsArticleEntity),
+          ),
+        );
+      },
+      child: Card(
+        margin: EdgeInsets.symmetric(
           horizontal: deviceWidth * 0.03,
-          vertical: deviceHeight * 0.015,
+          vertical: deviceHeight * 0.01,
         ),
-        child: Column(
-          children: [
-            Expanded(
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Padding(
-                      padding: const EdgeInsets.only(right: 16.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Flexible(
-                            child: AutoSizeText(
-                              title,
-                              maxLines: 4,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
+        child: Container(
+          height: deviceHeight >= deviceWidth
+              ? deviceHeight * 0.25
+              : deviceHeight * 0.5,
+          padding: EdgeInsets.symmetric(
+            horizontal: deviceWidth * 0.03,
+            vertical: deviceHeight * 0.015,
+          ),
+          child: Column(
+            children: [
+              Expanded(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                      flex: 2,
+                      child: Padding(
+                        padding: const EdgeInsets.only(right: 16.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Flexible(
+                              child: AutoSizeText(
+                                title,
+                                maxLines: 4,
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
                             ),
-                          ),
-                          SizedBox(height: 8.0),
-                          Expanded(
-                            child: Text(
-                              description,
-                              overflow: TextOverflow.ellipsis,
-                              maxLines: 4,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Expanded(
-                    flex: 1,
-                    child: Container(
-                      height: double.infinity,
-                      padding: EdgeInsets.symmetric(
-                        vertical: deviceHeight * 0.03,
-                      ),
-                      child: image != null
-                          ? ClipRRect(
-                              borderRadius: BorderRadius.circular(8.0),
-                              child: Image.network(
-                                image!,
-                                fit: BoxFit.cover,
-                                errorBuilder: (context, error, stackTrace) {
-                                  return const Center(child: Icon(Icons.error));
-                                },
+                            SizedBox(height: 8.0),
+                            Expanded(
+                              child: Text(
+                                description,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 4,
                               ),
-                            )
-                          : const Center(
-                              child: Icon(Icons.image_not_supported),
                             ),
+                          ],
+                        ),
+                      ),
                     ),
-                  ),
-                ],
+                    Expanded(
+                      flex: 1,
+                      child: Container(
+                        height: double.infinity,
+                        padding: EdgeInsets.symmetric(
+                          vertical: deviceHeight * 0.03,
+                        ),
+                        child: image != null
+                            ? ClipRRect(
+                                borderRadius: BorderRadius.circular(8.0),
+                                child: Image.network(
+                                  image!,
+                                  fit: BoxFit.cover,
+                                  errorBuilder: (context, error, stackTrace) {
+                                    return const Center(
+                                      child: Icon(Icons.error),
+                                    );
+                                  },
+                                ),
+                              )
+                            : const Center(
+                                child: Icon(Icons.image_not_supported),
+                              ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [Text(getTimeDifference())],
-            ),
-          ],
+              Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [Text(getTimeDifference())],
+              ),
+            ],
+          ),
         ),
       ),
     );
